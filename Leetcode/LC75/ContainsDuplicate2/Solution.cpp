28a50a82c122645f95f54e1f7103869355546a37
Ramya D <33391148+ramyaDhanush@users.noreply.github.com>
2022-10-08 18:53:50 +0530

    Create Solution.cpp

---
+++ Leetcode/LC75/ContainsDuplicate2/Solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int, int> m;
+        int n = nums.size();
+        for(int i=0; i<n; i++) {
+            if(m.count(nums[i])) {
+                if(i - m[nums[i]] <= k) return true;
+            }
+            m[nums[i]] = i;
+        }
+        
+        return false;
+    }
+};
